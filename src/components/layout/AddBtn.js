b037a234bc60abd47d6524eab1f3d78f815c7e92
semyers189 <semyers@fullsail.edu>
2019-07-07 21:44:14 -0400

    created technician modals

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -10,13 +10,13 @@ const AddBtn = () => {
         <i className='large material-icons'>add</i>
       </a>
       <ul>
-        <li className='btn-floating green modal-trigger'>
-          <a href='#tech-list-modal'>
+        <li className='btn-floating green'>
+          <a href='#tech-list-modal' className='modal-trigger'>
             <i className='material-icons'>person</i>
           </a>
         </li>
-        <li className='btn-floating red modal-trigger'>
-          <a href='#tech-modal'>
+        <li className='btn-floating red'>
+          <a href='#add-tech-modal' className='modal-trigger'>
             <i className='material-icons'>person_add</i>
           </a>
         </li>
